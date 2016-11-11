3c37615ce76b89342ce19657d39e5cacce5eb124
dukehm <dukehm@mail.lipscomb.edu>
2016-11-11 17:18:25 -0600

    Add files via upload

--- pgm.java
+++ pgm.java
@@ -1,83 +1,88 @@
-import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+/**
+ * Created by Miller on 11/11/2016.
+ */
+public class PGM extends Image {
+    public PGM(String s) {
+        super();
+        magic = s;
+    }
 
-public class pgm 
-{
+    public static void main(String[] args) throws FileNotFoundException {
 
-	public abstract void flip_horizontally()
-	{
-		int [][] horizdata = new int[width][height];
-		for(int k = 0; k < width;k++){
-			
-			for(int l = height - 1;l > -1; l--){
-				int numzero = 0;
-				horizdata[k][numzero] = data[k][l];
+        Scanner sc = new Scanner(new File("pgmfile.txt"));
+        String magic = sc.nextLine();
 
-				//System.out.print(horizdata[k][numzero] + " ");
-				numzero += 1;
-			}
-			
-			//System.out.println("");
-		}
-	}
+        String matr = sc.nextLine();
+        String[] splitmatr = matr.split("\\s+");
+        //System.out.println(splitmatr[0] + "//////" + splitmatr[1]);
+        int width = Integer.parseInt(splitmatr[1]);
+        int height = Integer.parseInt(splitmatr[0]);
+        int scale = sc.nextInt();
+        //System.out.println(scale);
+        int [][] data = new int[width][height];
 
-	public abstract void flip_vertically()
-	{
-			int [][] vertdata = new int[width][height];
-		
-			for(int m = width - 1; m > -1;m--)
-			{
-				for(int n = 0; n < height; n++)
-				{
-					vertdata[m][n] = data[m][n];
-					//System.out.print(vertdata[m][n] + " ");
-				}
-				//System.out.println(" ");
-			}
-	}
+        for(int i = 0; i < width;i++)
+        {
+            for(int j = 0; j < height;j++)
+            {
+                data[i][j] = sc.nextInt();
+                //System.out.print(data[i][j] + " ");
+            }
+            //System.out.println(" ");
+        }
+        //System.out.println("");
 
-	public abstract void rotate_right_90()
-	{
-		int [][] vertdata = new int[height][width];
-		
-		for(int m = 0; m < height;m++)
-		{	
-			for(int n = width - 1; n > -1; n--)
-			{
-				vertdata[m][n] = data[n][m];
-				//System.out.print(vertdata[m][n] + " ");
-			}
-			//System.out.println(" ");
-			
-		}
+    }
 
-	}
-	public static void main(String[] args) throws FileNotFoundException
-	{
-		Scanner sc = new Scanner(new File("pgmfile.txt"));
-		String magic = sc.nextLine();
+    @Override
+    public void flip_horizontally() {
+        int [][] horizdata = new int[width][height];
+        for(int k = 0; k < width;k++){
 
-		String matr = sc.nextLine();
-		String[] splitmatr = matr.split("\\s+");
-		//System.out.println(splitmatr[0] + "//////" + splitmatr[1]);
-		int width = Integer.parseInt(splitmatr[1]);
-		int height = Integer.parseInt(splitmatr[0]);
-		int scale = sc.nextInt();
-		//System.out.println(scale);
-		int [][] data = new int[width][height];
+            for(int l = height - 1;l > -1; l--) {
+                int numzero = 0;
+                
+                horizdata[k][numzero] = data[k][l];
 
-		for(int i = 0; i < width;i++)
-		{
-			for(int j = 0; j < height;j++)
-			{
-			data[i][j] = sc.nextInt();
-			//System.out.print(data[i][j] + " ");
-			}
-			//System.out.println(" ");
-		}
-		//System.out.println("");
-		
-	}
+                //System.out.print(horizdata[k][numzero] + " ");
+                numzero += 1;
+            }
+        }
+    }
+
+    @Override
+    public void flip_vertically() {
+
+        int [][] vertdata = new int[width][height];
+        for(int m = width - 1; m > -1;m--)
+        {
+            for(int n = 0; n < height; n++)
+            {
+                vertdata[m][n] = data[m][n];
+                //System.out.print(vertdata[m][n] + " ");
+            }
+            //System.out.println(" ");
+        }
+    }
+
+    @Override
+    public void rotate_right_90() {
+        int [][] vertdata = new int[height][width];
+
+        for(int m = 0; m < height;m++)
+        {
+            for(int n = width - 1; n > -1; n--)
+            {
+                vertdata[m][n] = data[n][m];
+                //System.out.print(vertdata[m][n] + " ");
+            }
+            //System.out.println(" ");
+
+        }
+    }
 }
+
